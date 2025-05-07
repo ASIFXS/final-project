fe1ec9ba38601c753d055c1efa4524e60f327e39
Asif Ali <162771045+ASIFXS@users.noreply.github.com>
2025-05-07 13:53:21 +0530

    Delete zrosa_ws directory

--- zrosa_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/asifali/zrosa_ws/src
-# Build directory: /home/asifali/zrosa_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("llm-ur-control/ur_agent")
